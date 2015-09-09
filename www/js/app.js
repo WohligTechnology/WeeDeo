cee0b170c01d81d27e3fc228ca8e026b4519f22a
Chintan Shah <chintan@wohlig.com>
2015-09-09 16:45:23 +0530

    side menu done

--- www/js/app.js
+++ www/js/app.js
@@ -120,5 +120,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/app/home');
 });
