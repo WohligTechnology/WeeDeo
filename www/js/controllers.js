e20f9c8c5b64239e6eb76823cf7e9b18798056f9
mahesh maurya <mahesh@wohlig.com>
2015-09-16 18:47:13 +0530

    home page done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,38 @@ angular.module('starter.controllers', [])
 .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
 })
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope, $ionicScrollDelegate, $stateParams) {
+    
+            //        ***** tabchange ****
+
+        $scope.tab = 'latest';
+        $scope.classa = 'active';
+        $scope.classb = '';
+        $scope.classc = '';
+        $scope.tabchange = function(tab, a) {
+            //        console.log(tab);
+            $scope.tab = tab;
+            if (a == 1) {
+                $ionicScrollDelegate.scrollTop();
+                $scope.classa = "active";
+                $scope.classb = '';
+                $scope.classc = '';
+            } else if (a == 2) {
+                $ionicScrollDelegate.scrollTop();
+                $scope.classa = '';
+                $scope.classb = "active";
+                $scope.classc = '';
+            } else {
+                $ionicScrollDelegate.scrollTop();
+                $scope.classa = '';
+                $scope.classb = '';
+                $scope.classc = "active";
+            }
+        };
+
+
+        //            ******** end *******
+
 
     })  
     .controller('LoginCtrl', function ($scope) {
